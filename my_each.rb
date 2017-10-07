5f52d56246e437bd259a592e670c52b32b6af690
Pamela Frieze <pfrieze@gmail.com>
2017-10-07 23:59:02 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(words) # put argument(s) here
   # code here
   i = 0
   while i <words.length
-  ['hi', 'hello', 'bye', 'goodbye'] do |words|
-
+    yield(collection[i])
+i = i + 1
 end
 end
