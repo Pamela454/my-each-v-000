33e64d173d1e281a921055677792d0b5fa20187e
Pamela Frieze <pfrieze@gmail.com>
2017-10-08 00:13:57 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(words) # put argument(s) here
   # code here
   i = 0
   while i < words.length
-    yield(words[i])
+    yield(words[i]) # stop executing the method and instead execute this code 
 i = i + 1
 end
 words
